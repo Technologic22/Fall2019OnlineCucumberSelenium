8e6fca60922a4e495d138595d4c64485bee2c3b2
Technologic22 <biomuhammet@gmail.com>
2020-07-05 14:13:24 -0400

    Screenshot attachment if test failed

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -3,6 +3,9 @@ package com.vytrack.step_definitions;
 import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
     @Before (order = 2)
@@ -11,18 +14,25 @@ public class Hooks {
         Driver.getDriver().manage().window().maximize();
     }
 
-    @Before (value = "@driver", order = 2)
+    @Before (value = "@driver", order = 1)
     public void specialSetup(){
         System.out.println("Setup for Driver only");
     }
 
-    @After(value = "@driver", order = 1)
+    @After("@driver")
     public void specialTearDown(){
         System.out.println("Tear down for driver only");
     }
 
     @After
-    public void teardown(){
+    public void teardown(Scenario scenario){
+        //how to check if scenario is failed
+        if (scenario.isFailed()){
+            TakesScreenshot takesScreenshot= (TakesScreenshot) Driver.getDriver();
+            byte[] image=takesScreenshot.getScreenshotAs(OutputType.BYTES);
+            //attach screenshot to the report
+            scenario.embed(image, "image/png", scenario.getName());
+        }
         System.out.println("Test clean up");
         Driver.closeDriver();
     }
