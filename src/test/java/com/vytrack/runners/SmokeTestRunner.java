57c0a3a592cd1ed695d5ac891a1ef8abaaf82e18
Technologic22 <biomuhammet@gmail.com>
2020-08-08 20:33:59 -0400

    Smoke test running from profile!

---
+++ src/test/java/com/vytrack/runners/SmokeTestRunner.java
@@ -0,0 +1,20 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions( monochrome = true,
+        glue = "com/vytrack/step_definitions",
+        features = "src/test/resources/features/activities",
+        dryRun = false,
+        strict=false,
+        tags = "@smoke_test",
+        plugin = {
+                "html:target/smoke_test_default-report",
+                "json:target/cucumber2.json"
+        }
+)
+public class SmokeTestRunner {
+}
